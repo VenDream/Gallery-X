27493cd149bb9120794e781a4361280df3dcd575
VenDream <yeshenxue@qq.com>
2018-09-29 10:25:37 +0800

    fix(sh): fix pwd cmd

--- start_service.sh
+++ start_service.sh
@@ -5,9 +5,9 @@ rm -rf logs
 docker run -d \
   -it \
   -p 8000:8000 \
-  -v "/$(PWD)/logs:/app/logs" \
-  -v "/$(PWD)/var/server.config.json:/app/var/server.config.json" \
-  -v "/$(PWD)/var/static.config.json:/app/var/static.config.json" \
+  -v "/$(pwd)/logs:/app/logs" \
+  -v "/$(pwd)/var/server.config.json:/app/var/server.config.json" \
+  -v "/$(pwd)/var/static.config.json:/app/var/static.config.json" \
   --rm \
   --name Gallery-X \
   vendream/gallery-x:latest
