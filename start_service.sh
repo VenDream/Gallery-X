9a617c931b4f796a8f53251f579f70c14562a95a
VenDream <yeshenxue@qq.com>
2018-09-29 10:18:25 +0800

    fix(docker): adjust docker run cmd

--- start_service.sh
+++ start_service.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+rm -rf logs
+
 docker run -d \
   -it \
   -p 8000:8000 \
-  -v "$(pwd -W)/var:/app/var" \
+  -v "/$(PWD)/logs:/app/logs" \
+  -v "/$(PWD)/var/server.config.json:/app/var/server.config.json" \
+  -v "/$(PWD)/var/static.config.json:/app/var/static.config.json" \
+  --rm \
   --name Gallery-X \
   vendream/gallery-x:latest
