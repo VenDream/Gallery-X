bcfa8d1bc9752d0696e4495c9168cd58aa2d2733
VenDream <yeshenxue@qq.com>
2018-11-22 19:16:08 +0800

    feat: use docker-compose to manage containers

--- start_service.sh
+++ start_service.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
 # 拉取最新镜像
-docker pull vendream/gallery-x:latest
-# 停掉原有容器
-docker stop Gallery-X
-# 从新镜像启动新的容器
-docker run -d \
-  -it \
-  -p 8000:8000 \
-  -v "/$(pwd)/logs:/app/logs" \
-  -v "/$(pwd)/var/server.config.json:/app/var/server.config.json" \
-  -v "/$(pwd)/var/static.config.json:/app/var/static.config.json" \
-  --rm \
-  --name Gallery-X \
-  vendream/gallery-x:latest
+docker-compose pull
+# 重启服务
+docker-compose down
+docker-compose up -d
