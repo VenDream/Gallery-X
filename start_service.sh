74869a2fe435c5222916fd06bb226ee5fec6882d
VenDream <yeshenxue@qq.com>
2019-03-22 18:26:24 +0800

    chore(build): update start script

--- start_service.sh
+++ start_service.sh
@@ -2,6 +2,11 @@
 
 # 拉取最新镜像
 docker-compose pull
-# 重启服务
-docker-compose down
+# 停止原服务
+docker-compose down -v
+# 启动服务
 docker-compose up -d
+# 删除无用镜像
+yes | docker image prune
+# 删除无用容器
+yes | docker container prune
